3e514003548a32736bc3e3033cfb922c0365c65c
aiishine <hadhadaicha@gmail.com>
2023-10-17 11:22:33 +0100

    adding categories and subcategories schemas

---
+++ Backend/Models/Categories.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const categoriesSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    category_name: {
+        type: String,
+        required: true
+    },
+    active: {
+        type: Boolean,
+        required: true
+    }
+}, 
+{ timestamps: true });
+
+module.exports = mongoose.model("Categories", categoriesSchema);
